75046b24e783bc83e2b8419766909afa4fd610d2
I-GALLA <igalla@outlook.fr>
2020-08-03 22:39:02 +0200

    upd : add join pm receiver model

--- app/models/private_message.rb
+++ app/models/private_message.rb
@@ -1,5 +1,7 @@
 class PrivateMessage < ApplicationRecord
     belongs_to :sender, class_name: "User"
-    belongs_to :recipient, class_name: "User"
+    has_many :join_pm_receivers
+    has_many :recipients, class_name: "User", through: :join_pm_receivers
 end
+  
   
