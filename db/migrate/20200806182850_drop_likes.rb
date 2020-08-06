12f300f3a068dac7f43bff8fc338967d596ece8d
I-GALLA <igalla@outlook.fr>
2020-08-06 20:33:50 +0200

    upd table likes updated

---
+++ db/migrate/20200806182850_drop_likes.rb
@@ -0,0 +1,6 @@
+class DropLikes < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :likes
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_203250) do
+ActiveRecord::Schema.define(version: 2020_08_06_183138) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
@@ -57,8 +57,13 @@ ActiveRecord::Schema.define(version: 2020_08_03_203250) do
   end
 
   create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "content_type"
+    t.integer "content_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["content_type", "content_id"], name: "index_likes_on_content_type_and_content_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "private_messages", force: :cascade do |t|
